b4e6677e373dd914898e25cec877c56e6b84e838
xpercy29 <spangeled@gmail.com>
2024-03-06 00:48:37 +0530

    added user.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const userRouter = require("./user");
 
 const router = express.Router();
+
+router.use("/user", userRouter);
 
 modeule.exports = router;
 //api/v1/user
