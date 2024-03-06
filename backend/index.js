7bed34de0e98fc07a9e5cfc7737440d85083fe1b
xpercy29 <spangeled@gmail.com>
2024-03-06 05:47:28 +0530

    added cors

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
-const rootRouter = require("./routes/index");
+const cors = require("cors");
+const mainRouter = require("./routes/index");
 
 const app = express();
 
-app.use("/api/v1", rootRouter);
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/v1", mainRouter);
