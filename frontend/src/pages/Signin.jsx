1cc3304ffa883ae7e858d69a08936e353693f23a
xpercy29 <spangeled@gmail.com>
2024-03-18 14:47:22 +0530

    added toastify to signup

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -6,10 +6,12 @@ import { InputBox } from "../components/InputBox";
 import { SubHeading } from "../components/SubHeading";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export const Signin = () => {
   const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const [password, setPassword] = useState("");  
   const navigate = useNavigate();
 
   return (
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -6,6 +6,8 @@ import { InputBox } from "../components/InputBox";
 import { SubHeading } from "../components/SubHeading";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export const Signup = () => {
   const [firstName, setFirstName] = useState("");
@@ -15,62 +17,73 @@ export const Signup = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="bg-slate-300 h-screen flex justify-center">
-      <div className="flex flex-col justify-center">
-        <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
-          <Heading label={"Sign up"} />
-          <SubHeading label={"Enter your infromation to create an account"} />
-          <InputBox
-            onChange={(e) => {
-              setFirstName(e.target.value);
-            }}
-            placeholder="samarth"
-            label={"First Name"}
-          />
-          <InputBox
-            onChange={(e) => {
-              setLastName(e.target.value);
-            }}
-            placeholder="sharma"
-            label={"Last Name"}
-          />
-          <InputBox
-            onChange={(e) => {
-              setUsername(e.target.value);
-            }}
-            placeholder="samarth@gmail.com"
-            label={"Email"}
-          />
-          <InputBox
-            onChange={(e) => {
-              setPassword(e.target.value);
-            }}
-            placeholder="123456"
-            label={"Password"}
-          />
-          <div className="pt-4">
-            <Button
-              onClick={async () => {
-                const response = await axios.post(
-                  "http://localhost:3000/api/v1/user/signup",
-                  {
-                    username,
-                    firstName,
-                    lastName,
-                    password,
-                  }
-                );
-                localStorage.setItem("token", response.data.token);
-                navigate("/dashboard");
+    <div>
+      <ToastContainer autoClose={500} hideProgressBar={true} />
+
+      <div className="bg-slate-300 h-screen flex justify-center">
+        <div className="flex flex-col justify-center">
+          <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
+            <Heading label={"Sign up"} />
+            <SubHeading label={"Enter your infromation to create an account"} />
+            <InputBox
+              onChange={(e) => {
+                setFirstName(e.target.value);
               }}
-              label={"Sign up"}
+              placeholder="samarth"
+              label={"First Name"}
+            />
+            <InputBox
+              onChange={(e) => {
+                setLastName(e.target.value);
+              }}
+              placeholder="sharma"
+              label={"Last Name"}
+            />
+            <InputBox
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
+              placeholder="samarth@gmail.com"
+              label={"Email"}
+            />
+            <InputBox
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+              placeholder="123456"
+              label={"Password"}
+            />
+            <div className="pt-4">
+              <Button
+                onClick={async () => {
+                  try {
+                    const response = await axios.post(
+                      "http://localhost:3000/api/v1/user/signup",
+                      {
+                        username,
+                        firstName,
+                        lastName,
+                        password,
+                      }
+                    );
+                    localStorage.setItem("token", response.data.token);
+                    toast.success("Succesfully Signed up! :)", {
+                      autoClose: 3000,
+                    });
+                    navigate("/dashboard");
+                  } catch (error) {
+                    toast.error("Error while Signing up! :(");
+                  }
+                }}
+                label={"Sign up"}
+              />
+            </div>
+            <BottomWarning
+              label={"Already have an account?"}
+              buttonText={"Sign in"}
+              to={"/signin"}
             />
           </div>
-          <BottomWarning
-            label={"Already have an account?"}
-            buttonText={"Sign in"}
-            to={"/signin"}
-          />
         </div>
       </div>
     </div>
